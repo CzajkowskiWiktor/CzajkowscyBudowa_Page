d7eb47444e2cac9be07cecc0711d6c3388104c00
Wiktor Czajkowski <malyczaja12@gmail.com>
2020-09-04 20:21:21 +0200

    add team section with style and imgs

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
+    //add shadow do navigation menu
     const nav = document.querySelector('.navbar');
     function addShadow() {
         if (window.scrollY > 400) {
@@ -18,4 +19,34 @@ document.addEventListener('DOMContentLoaded', function () {
             }
         });
     });
+    //slickJS
+    $(document).ready(function(){
+        $('.team-carousel').slick({
+            autoplay: true,
+            autoplaySpeed: 4000,
+            mobileFirst: true,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            responsive: [
+                {
+                    breakpoint: 768,
+                    settings: {
+                        slidesToShow: 2
+                    }
+                },
+                {
+                    breakpoint: 992,
+                    settings: {
+                        slidesToShow: 3
+                    }
+                },
+                {
+                    breakpoint: 1580,
+                    settings: {
+                        slidesToShow: 4
+                    }
+                }
+            ]
+        });
+      });
 });
